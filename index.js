2d96a34dc7569dd14e1a1aa29d0d15fc3e1c3f53
Somil <somilarora55@gmail.com>
2024-11-01 17:08:19 -0400

    updating all the files

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express"
+
+import appConfig from './config/appConfig.js'
+import router from "./router/router.js"
+
+const {PORT} = appConfig
+const app = express()
+
+app.use(express.json())
+app.use('/',router)
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
+  })
