d8d913c2b92fefafa2310510f574177f6d532973
Somil <somilarora55@gmail.com>
2024-11-02 17:44:25 -0400

    Delete service directory

--- service/googleSheet.js
+++
@@ -1,9 +0,0 @@
-import { google } from "googleapis";
-import appConfig from "../config/appConfig";
-
-const {GOOGLE_SHEET_CLIENT_EMAIL,GOOGLE_SHEET_PRIVATE_KEY} = appConfig
-
-
-const scopes = ['https://www.googleapis.com/auth/spreadsheets']
-
-const sheetClient = new google.auth.JWT(GOOGLE_SHEET_CLIENT_EMAIL,null,GOOGLE_SHEET_PRIVATE_KEY,scopes)
